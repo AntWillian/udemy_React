b5c8a9b853bcf64b525e6d1688772ddc0baa8173
AntWillian <antonio.lima@carriers.com.br>
2019-01-21 13:15:03 -0200

    Component

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,1 +1,17 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import Titulo from './titulo/titulo';
+import Navbar from './navbar/navbar';
+
+let App = (
+    <div>
+        <Navbar titulo="Component" cor="orange"/>
+        <div>
+            <Titulo />
+        </div>
+    </div>
+);
+
+ReactDOM.render(App, document.getElementById('app'));
 
